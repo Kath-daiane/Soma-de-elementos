c541eb8d07960cede7cf73a1614d06cecf153dda
Kath-daiane <kath.daiane.p@gmail.com>
2025-02-27 10:07:27 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Criar o array com 5 elementos
+        int[] arraySoma = { 10, 20, 30, 40, 50 };
+
+        // Inicializar a variável soma
+        int soma = 0;
+
+        // Somar os elementos do array
+        for (int i = 0; i < arraySoma.Length; i++)
+        {
+            soma = soma + arraySoma[i];
+        }
+
+        // Exibir o resultado
+        Console.WriteLine("A soma de todos os elementos é: " + soma);
+    }
+}
